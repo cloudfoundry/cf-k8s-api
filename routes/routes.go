a9f901ded4ef7756db6ffa3704821dee2cbf80fd
Ashwin Krishna <krishnaas@vmware.com>
2021-08-31 12:20:33 -0700

    WIP - register create apps route with apirouter
    Co-authored-by: Dave Walter <walterda@vmware.com>

--- routes/routes.go
+++ routes/routes.go
@@ -5,25 +5,27 @@ import (
 	"net/http"
 )
 
-
 // Just contains the CF API Routes and maps them to handler functions
 
 const (
-	RootGetEndpoint = "/"
-	RootV3GetEndpoint = "/v3"
+	RootGetEndpoint    = "/"
+	RootV3GetEndpoint  = "/v3"
+	AppsCreateEndpoint = RootV3GetEndpoint + "/apps"
 )
 
 type httpHandlerFunction func(w http.ResponseWriter, r *http.Request)
 
 type APIRoutes struct {
-	RootV3Handler httpHandlerFunction
-	RootHandler httpHandlerFunction
+	RootV3Handler     httpHandlerFunction
+	RootHandler       httpHandlerFunction
+	AppsCreateHandler httpHandlerFunction
 }
 
-func (a *APIRoutes) RegisterRoutes( router *mux.Router ) {
-	if a.RootV3Handler == nil || a.RootHandler == nil {
+func (a *APIRoutes) RegisterRoutes(router *mux.Router) {
+	if a.RootV3Handler == nil || a.RootHandler == nil || a.AppsCreateHandler == nil {
 		panic("APIRoutes: handler was nil")
 	}
 	router.HandleFunc(RootGetEndpoint, a.RootHandler).Methods("GET")
 	router.HandleFunc(RootV3GetEndpoint, a.RootV3Handler).Methods("GET")
+	router.HandleFunc(AppsCreateEndpoint, a.AppsCreateHandler).Methods("POST")
 }
