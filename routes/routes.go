8f63675256891a478d5c1994930576881c74d4b2
Ashwin Krishna <krishnaas@vmware.com>
2021-08-31 12:20:33 -0700

    WIP - rebase with main

--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,7 @@ const (
 	RootGetEndpoint   = "/"
 	RootV3GetEndpoint = "/v3"
 	AppsGetEndpoint   = "/v3/apps/{guid}"
+	AppsCreateEndpoint = RootV3GetEndpoint + "/apps"
 )
 
 type httpHandlerFunction func(w http.ResponseWriter, r *http.Request)
@@ -20,13 +21,15 @@ type APIRoutes struct {
 	RootV3Handler httpHandlerFunction
 	RootHandler   httpHandlerFunction
 	AppsHandler   httpHandlerFunction
+	AppsCreateHandler httpHandlerFunction
 }
 
 func (a *APIRoutes) RegisterRoutes(router *mux.Router) {
-	if a.RootV3Handler == nil || a.RootHandler == nil || a.AppsHandler == nil {
+	if a.RootV3Handler == nil || a.RootHandler == nil || a.AppsHandler == nil || a.AppsCreateHandler == nil{
 		panic("APIRoutes: handler was nil")
 	}
 	router.HandleFunc(RootGetEndpoint, a.RootHandler).Methods("GET")
 	router.HandleFunc(RootV3GetEndpoint, a.RootV3Handler).Methods("GET")
+	router.HandleFunc(AppsCreateEndpoint, a.AppsCreateHandler).Methods("POST")
 	router.HandleFunc(AppsGetEndpoint, a.AppsHandler).Methods("GET")
 }
